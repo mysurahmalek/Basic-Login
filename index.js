f52599ff018ff0991898ac6f5f9b9e4dafd8de25
Your Name <you@example.com>
2023-03-31 09:13:32 +0800

    function login

---
+++ index.js
@@ -0,0 +1,45 @@
+let dbUsers = [
+    {
+        username: "soo",
+        password: "password",
+        name: "Soo Yew Guan",
+        email: "soo@utem.edu.my"
+    },
+    {
+        username: "wee",
+        password: "password",
+        name: "Abu",
+        email: "soo@utem.edu.my"
+    },
+    {
+    username: "que",
+    password: "password",
+    name: "Ali",
+    email: "soo@utem.edu.my"
+    }
+]
+
+// Purpose to sent request to cloud about the detail of login
+function login (username, password) {
+    console.log("Someone try to login with", username, password)
+    let matched = dbUsers.find(element => {
+        element.username == username
+    })
+        //console.log(matched)
+        if(matched)
+        {
+             if(matched.password == password){
+                return matched
+             }else{
+                return "Password not matched"
+             }
+        }
+        else {
+            return "User not found"
+        }
+}
+
+// Call function (give parameter) try to login
+// login("soo", "password")
+// login("soo", "123456")
+console.log(login("soo","password"))
